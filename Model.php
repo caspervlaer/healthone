06c14cbcbaa6eee57242cc356caf17456a66fc53
caspervlaer <56535349+caspervlaer@users.noreply.github.com>
2020-11-06 16:45:30 +0100

    Add files via upload

--- Model.php
+++ Model.php
@@ -1,16 +1,21 @@
 <?php
 namespace model;
 use model\Drug;
-include_once ("model/Drug.php");
+include_once("model/Drug.php");
+use model\Patient;
+include_once ("model/Patient.php");
 
 class Model
 {
     private $drug;
+    private $patient;
+    private $user;
     public $content;
+    private $database;
 
     public function __construct()
     {
-        $this->content = "<form method='post'>
+        $this->content = "<form method='post' action='index.php'>
                            Name: <input type='text' name='name'>
                            Wachtwoord: <input type='text' name='wacht'>
                            <input type='submit'>";
@@ -18,6 +23,45 @@ class Model
 
     private function makeConnection(){
         $this->database = new \PDO('mysql:host=localhost;dbname=healthone', "root", "");
+    }
+
+    public function insertUser($naam,$wacht,$roles){
+        $this->makeConnection();
+        if($naam !='')
+        {
+            $query = $this->database->prepare (
+                "INSERT INTO `users` (`id`, `naam`, `wacht`, `roles`) 
+                VALUES (NULL, :naam, :wacht, :roles)");
+            $query->bindParam(":naam", $naam);
+            $query->bindParam(":wacht", strtoupper(hash("sha256", $wacht)));
+            $query->bindParam(":roles", $roles);
+            $result = $query->execute();
+            return $result;
+        }
+        return -1;
+    }
+
+    public function login($username, $wachtwoord){
+        $this->makeConnection();
+        $selection = $this->database->prepare(
+            'SELECT * FROM `users` WHERE `users`.`username` =:username');
+        $selection->bindParam(":username",$username);
+        $result = $selection ->execute();
+        if ($result){
+            $selection->setFetchMod(\PDO::FETCH_CLASS, \model\User::class);
+            $user = $selection->fetch();
+            if ($user){
+                $gehashtpassword = strtoupper(hash(" "), $wachtwoord);
+                if ($user->getWachtwoord() == $gehashtpassword){
+                    $_SESSION['user']=$user->getName();
+                    $_SESSION['roles']=$user->getRole();
+                }
+            }
+        }
+    }
+    public function logout(){
+        session_unset();
+        session_destroy();
     }
 
     public function getDrug()
@@ -38,6 +82,11 @@ class Model
     public function setContent($content)
     {
         $this->content = $content;
+    }
+
+    public function setUser($name, $wachtwoord, $role, $id)
+    {
+        $this->user = new User($name, $wachtwoord, $role, $id);
     }
 
 }
